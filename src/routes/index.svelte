52077cde360f8cd77574e26a7bceafe264b0532f
Johan S. Steinberg <steinberg@hey.com>
2022-02-27 11:14:57 +0100

    Move items html files to route as markdown files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Items</h1>
+
+<ul>
+	{#each items as item}
+		<li>
+			<a
+				id={item.title.toLowerCase()}
+				href={item.path}
+				sveltekit:prefetch
+			>
+				{item.title}
+			</a>
+		</li>
+	{/each}
+</ul>
+
+<script>
+	export let items;
+</script>
+
+<script context="module">
+	const mdFiles = import.meta.globEager(`./*.md`);
+	const getSlug = (path) => path.replace(/.*\/([^/]*)\..*$/, "$1");
+
+	export const load = async ({ url }) => {
+		const data = Object.keys(mdFiles)
+			.map((path) => {
+				return {
+					filePath: path,
+					slug: getSlug(path),
+					path: `/${getSlug(path)}`,
+					title: mdFiles[path].metadata?.title || getSlug(path),
+					metadata: mdFiles[path].metadata,
+				};
+			});
+
+		return {
+			props: {
+				items: data,
+			},
+		};
+	};
+</script>
